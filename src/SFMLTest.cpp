8a4290b3f8de1d2100fef22dc28a2565156430fd
edodo7 <massa-messo@hotmail.com>
2016-09-25 20:19:20 +0200

    initial commit

---
+++ src/SFMLTest.cpp
@@ -0,0 +1,28 @@
+// SFMLTest.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
